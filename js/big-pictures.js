99d6edad6bb1667fbc0e9ee67becb365a3e15c77
Музаффар <muzaffarmuz@yandex.ru>
2023-04-10 17:35:27 +0300

    Убирает ошибку открытия модалки

--- js/big-pictures.js
+++ js/big-pictures.js
@@ -41,6 +41,9 @@ const renderComments = (comments, node) => {
 //Открывает большую фотографию миниатюры
 const openBigPicture = (data) => {
   //console.log(data.dataset.photoId, 'Данные карточки');
+  if (!data) {
+    return;
+  }
   const photo = photoData.find((item) => item.id === +data.dataset.photoId);
   //console.log(photo, 'photo');
   document.querySelector('body').classList.add('modal-open');
